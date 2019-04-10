56b33a3e6370642aca2f126c0717147fbd6999db
dangfuli <623037035@qq.com>
2019-04-10 20:04:31 +0800

    新建悦小说api项目

---
+++ yxs-api/runner/runner.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+import time,os
+import unittest
+from lib.HTMLTestRunner import HTMLTestRunner
+
+
+def get_test():
+    '''
+    加载用例
+    :return:
+    '''
+    case_path = os.path.join(os.path.abspath(".."),"case")
+    print(case_path)
+    discover = unittest.defaultTestLoader.discover(case_path,pattern="*.py")
+    testcase = unittest.TestSuite()   # 创建单元套件，添加用例到套件
+    for test_suit in discover:
+        for i in test_suit:
+            testcase.addTest(i)
+    return testcase
+
+def html_report(report_path=os.path.join(os.path.abspath(".."),"report")):
+    ## 拼接输出报告的路径
+    report = time.strftime("%m-%d-%H-%M",time.localtime())
+    path = report_path + "\\" + report + ".html"
+    ## 打开报告
+    f = open(path,"wb")
+    runner = HTMLTestRunner(stream=f,title="test-report",description="run of description")
+    runner.run(get_test())
+    f.close()
+if __name__ == '__main__':
+   # installApp()
+    html_report()
